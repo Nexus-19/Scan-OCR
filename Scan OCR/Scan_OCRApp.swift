e26311552c99f3359392d62d69627aa64815606c
Soumyattam Dey <soumyattamdey14@gmail.com>
2022-01-27 17:22:34 +0530

    Initial Commit

---
+++ Scan OCR/Scan_OCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_OCRApp.swift
+//  Scan OCR
+//
+//  Created by Soumyattam Dey on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Scan_OCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
